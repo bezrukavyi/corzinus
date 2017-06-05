6d55dda8db4a545256e9a7aa33cb5f038d36a960
Yaroslav Bezrukavyi <yaroslav555@gmail.com>
2017-06-05 21:20:09 +0300

    Test

--- app/models/corzinus/inventory_analysis.rb
+++ app/models/corzinus/inventory_analysis.rb
@@ -3,7 +3,6 @@ module Corzinus
     validates :inventory_data, :demand_data, :price_data, :delivery_cost,
               :warehous_cost, :reserves, :delivery_days, presence: true
 
-
     def analysed_reserves
       @analysed_reserves ||= BestReserveValue.new(attributes).all_profits
     end
